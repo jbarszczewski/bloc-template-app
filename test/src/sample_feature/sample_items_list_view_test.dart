30cf27b1b2418627344b825de1e504286db26d65
jbarszczewski <jakub.barszczewski@outlook.com>
2024-06-14 10:28:45 +0200

    add License and fix tests

--- test/src/sample_feature/sample_items_list_view_test.dart
+++ test/src/sample_feature/sample_items_list_view_test.dart
@@ -1,13 +1,14 @@
 import 'package:bloc_template_app/src/sample_feature/sample_items_list_view.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('items list view has title from translations', (tester) async {
     // Add test code here.
-    await tester.pumpWidget(const SampleItemsListView(
+    await tester.pumpWidget(const MaterialApp(
+        home: SampleItemsListView(
       items: [],
-    ));
-
+    )));
     expect(find.text('Items'), findsOneWidget);
   });
 }
