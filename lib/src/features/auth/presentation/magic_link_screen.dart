7c5c25c4e83e57a07de87ca519dfb35e54fad407
jbarszczewski <jakub.barszczewski@outlook.com>
2024-09-18 11:52:41 +0200

    Add flutter_flavorizr package for flavor management

---
+++ lib/src/features/auth/presentation/magic_link_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MagicLinkScreen extends StatelessWidget {
+  const MagicLinkScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Magic Link Screen'),
+    );
+  }
+}
